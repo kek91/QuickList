18698325bce4580a2f6d48f9ca6bcd8498fcb3ff
KEK <kek@teknix.no>
2015-07-12 15:02:53 +0200

    Implemented tabs for 2 todo lists. Various other changes and bugfixes

--- www/js/quicklist.js
+++ www/js/quicklist.js
@@ -1,18 +1,29 @@
 $(document).bind("pageinit", function() {
-    
-    //var test = localStorage.getItem("Hello wørld");
+
+    // Populating existing tasks from localStorage
     for (var i = 0; i < localStorage.length; i++){
         var todoEntry = localStorage.getItem(localStorage.key(i));
         if(todoEntry != "undefined") {
-            $('#todolist').append('<label><input type="checkbox" name="todo[]" value="'+todoEntry+'">'+todoEntry+'</label>');  
+            if(todoEntry.indexOf('||1||') == 0) {
+                var todoEntryText = todoEntry.replace('||1||', '');
+                $('#todolist').append('<label><input type="checkbox" name="todo[]" value="'+todoEntry+'">'+todoEntryText+'</label>');  
+            }
+            else {
+                var todoEntryText = todoEntry.replace('||2||', '');
+                $('#todolist2').append('<label><input type="checkbox" name="todo[]" value="'+todoEntry+'">'+todoEntryText+'</label>');  
+            }
         }
-        // $('#todolist').append(localStorage.getItem(localStorage.key(i)));
         $('input[type=checkbox]').checkboxradio().trigger('create');
     }
 
+
+
+
+    /* Todo list 1 */
     $('form#todo_add').submit(function(event){
         event.preventDefault();
-        var data = $('input[name=todo_add_item]').val().trim();
+        var data = '||1||' + $('input[name=todo_add_item]').val().trim();
+        var dataText = $('input[name=todo_add_item]').val().trim();
         if(!data) {
             alert("Text input cannot be empty.");
         }
@@ -21,13 +32,12 @@ $(document).bind("pageinit", function() {
         }
         else {
             localStorage.setItem(data, data);
-            $('#todolist').append('<label><input type="checkbox" name="todo[]" value="'+data+'">'+data+'</label>');
+            $('#todolist').append('<label><input type="checkbox" name="todo[]" value="'+data+'">'+dataText+'</label>');
             $('input[type=checkbox]').checkboxradio().trigger('create');
             $('input[name=todo_add_item]').val("");
+            $('input[name=todo_add_item]').blur();
         }
     });
-
-
     $('#todo_clean').click(function(){
         var selected = [];
         $('#todolist input:checked').each(function() {
@@ -37,60 +47,46 @@ $(document).bind("pageinit", function() {
         });
     });
 
+    // And then the same code again, but this time for the second todo list.
+
+    /* Todo list 2 */
+    $('form#todo_add2').submit(function(event){
+        event.preventDefault();
+        var data = $('input[name=todo_add_item2]').val().trim();
+        var dataText = $('input[name=todo_add_item2]').val().trim();
+        if(!data) {
+            alert("Text input cannot be empty.");
+        }
+        else if (localStorage.getItem(data)) {
+            alert("This entry already exists");
+        }
+        else {
+            localStorage.setItem(data, data);
+            $('#todolist2').append('<label><input type="checkbox" name="todo[]" value="||2||'+data+'">'+dataText+'</label>');
+            $('input[type=checkbox]').checkboxradio().trigger('create');
+            $('input[name=todo_add_item2]').val("");
+            $('input[name=todo_add_item2]').blur();
+        }
+    });
+    $('#todo_clean2').click(function(){
+        var selected = [];
+        $('#todolist2 input:checked').each(function() {
+            $(this).prev('label').remove();
+            $(this).remove();
+            localStorage.removeItem($(this).val());
+        });
+    });
+
+    // On a totally unrelated subject, I wonder what DRY means.
+
+
+
+    // Note, this deletes everything in localStorage, meaning both todo lists will be deleted
     $('#todo_delete_confirmed').click(function(){
         $('#todolist').html("");
+        $('#todolist2').html("");
         localStorage.clear();
     });
 
 
-    // $('label').click(function(){
-    //     $(this).addClass('grayedOut');
-    // });
-
-    // $(document).on('click', 'label', function(){
-    //     $(this).next('div').css('background', '000000');
-    // });
-     
-
-// $(document).on('click', '.list_accordion_toggle', function (event) {
-//     $(this).toggleClass('list_item collapse').next().toggle();
-//     $(this).find('.icon-minus-sign, .icon-plus-sign').toggleClass('icon-minus-sign icon-plus-sign')
-// });
-
-    
-    // $(document).on('click', 'p', function(){
-    //     //$(this).remove();
-    //     //$(this).toggleClass('grayedOut');
-    //     $(this).css('text-decoration', 'line-through');
-    // });
-
-
-    $(function(){
-        // Bind the swiperightHandler callback function to the swipe event on div.box
-      $( "p" ).on( "swiperight", swiperightHandler );
-     
-      // Callback function references the event target and adds the 'swiperight' class to it
-      function swiperightHandler( event ){
-        $( event.target ).addClass( "grayedOut" );
-      }
-    });
-
 });
-
-
-
-
-// $(document).ready(function() {
-
-//     $("#button").click(function() {
-//         var toAdd = $("input[name=checkListItem]").val();
-//         $(".list").append('<div class="item">' + toAdd + '</div>');
-//     })
-    
-//     $(document).on('click', '.item', function() {
-//         $(this).remove();
-//     })
-
-// })
-
-
